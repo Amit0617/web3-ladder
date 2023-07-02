ea37443902802903044ef5d27b7f52b44c161af1
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-07-02 20:29:42 +0530

    setup hardhat

--- contracts/MintNFT.sol
+++ contracts/MintNFT.sol
@@ -1,26 +1,28 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/drafts/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ORGNFT is ERC721Full, Ownable {
+contract ORGNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     // A url for the metadata or simply Organisation Logo
-    string memory tokenURI;
+    string metadataURI;
 
-    constructor(string memory _tokenURI) ERC721Full("ORGNFT", "ORG") public {
-        tokenURI = _tokenURI;
+    constructor(string memory _tokenURI) ERC721("ORGNFT", "ORG") {
+        metadataURI = _tokenURI;
     }
 
-    function mintNFT(address recipient) public returns (uint256) onlyOwner {
+    function mintNFT(address recipient) public onlyOwner returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, metadataURI);
 
         return newItemId;
     }
--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -1,9 +1,45 @@
+'use client';
+import { useState } from "react";
 import styles from "../page.module.css";
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import MintNFT from "../../artifacts/contracts/MintNFT.sol/MintNFT.json"
 
 export default function Mint(){
+
+    // Contract address
+    // TODO: Update this with the contract address from the deploy script
+    // There are actually multiple ideas for how to do this:
+    // 1. Hardcode the contract address
+    // 2. Prompt the user to enter the contract address
+    // 3. Create a multi Tenant contract that can be deployed on a click by the organisation that wants to use it
+    const [contractAddress, setContractAddress] = useState('')
+
+    const mintNFts = async () => {
+        // Connect to wallet
+        const web3Modal = new Web3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const signer = provider.getSigner()
+
+        // Get contract
+        let contract = new ethers.Contract(contractAddress, MintNFT.abi, signer)
+        let transaction = await contract.mintNFT()
+        
+        // Wait for transaction to finish and log it to console
+        let tx = await transaction.wait()
+        let event = tx.events[0]
+        console.log("event:", event)
+        let value = event.args[2]
+        let tokenId = value.toNumber()
+        console.log(tokenId)
+    }
+
     return <main className={styles.main}>
         <h1>Mint BULK NFTs</h1>
         {/* Large text area for inserting wallet address for each line */}
         <textarea rows={10} cols={40} placeholder="Put one wallet address in each line..."></textarea>
+        {/* Button to mint NFTs */}
+        <button onClick={mintNFts}>Mint NFTs</button>
     </main>;
 }
