59604441a132bc6398a7330a8293fd7fc75d8473
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-07-02 20:48:33 +0530

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const ipfsMetadataHash = "ipfs://hashOfMetadata";
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  const mintNFT = await ethers.deployContract("MintNFT", ipfsMetadataHash);
+  await mintNFT.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `MintNFT with ${ipfsMetadataHash} URL deployed to ${mintNFT.target}`
   );
 }
 
